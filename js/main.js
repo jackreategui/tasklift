bc9fe524c4954fcf1341256821ecbfbcb028765a
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-16 10:30:54 -0500

    agregue inputs para que los usuarios escriban en las tareas

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const cuentaIcono = document.querySelector('.cuenta_icon');
 const iconCerrar = document.querySelector('.icon--cerrar');
 const menuOverlay = document.querySelector('.menu_overlay');
 const menuContainer = document.querySelector('.container_menu');
+const plusIcon = document.querySelector('.plus_icon');
+const taskRow = document.querySelector('.task--row');
+const containerTask = document.querySelector('.container_task');
 
 cuentaIcono.addEventListener('click', () => {
   iconCerrar.classList.toggle('icon_cerrar--no');
@@ -13,4 +16,13 @@ menuOverlay.addEventListener('click', () => {
   iconCerrar.classList.remove('icon_cerrar--no');
   menuOverlay.classList.remove('menu_overlay--oculto');
   menuContainer.classList.remove('container_menu--abierto');
+})
+
+const filaTarea = [];
+filaTarea.push({
+
+});
+
+plusIcon.addEventListener('click', ()=>{
+  const html = 
 })
