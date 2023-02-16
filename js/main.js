8b74f13bb7d15a1ef5de3c44d9081e34b35b0046
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-15 19:52:05 -0500

    termine de darles estilos con css y js al menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 const cuentaIcono = document.querySelector('.cuenta_icon');
+const iconCerrar = document.querySelector('.icon--cerrar');
+const menuOverlay = document.querySelector('.menu_overlay');
+const menuContainer = document.querySelector('.container_menu');
+
+cuentaIcono.addEventListener('click', () => {
+  iconCerrar.classList.toggle('icon_cerrar--no');
+  menuOverlay.classList.toggle('menu_overlay--oculto');
+  menuContainer.classList.toggle('container_menu--abierto');
+})
+
+menuOverlay.addEventListener('click', () => {
+  iconCerrar.classList.remove('icon_cerrar--no');
+  menuOverlay.classList.remove('menu_overlay--oculto');
+  menuContainer.classList.remove('container_menu--abierto');
+})
