c4e393b7a8285262671d94ab7f5d29ba47b1e112
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-16 21:04:36 -0500

    agregue un avance y correccion a las tareas con js

--- js/main.js
+++ js/main.js
@@ -1,82 +1,78 @@
-const cuentaIcono = document.querySelector('.cuenta_icon');
-const iconCerrar = document.querySelector('.icon--cerrar');
-const menuOverlay = document.querySelector('.menu_overlay');
-const menuContainer = document.querySelector('.container_menu');
-const plusIcon = document.querySelector('.plus_icon');
-// const taskRow = document.querySelector('.task--row');
-const containerTask = document.querySelector('.container_task');
-// const marcadorTarea = document.querySelector('.no_marcado--task');
-// const textTarea = document.querySelector('.text--task');
+const cuentaIcono = document.querySelector(".cuenta_icon");
+const iconCerrar = document.querySelector(".icon--cerrar");
+const menuOverlay = document.querySelector(".menu_overlay");
+const menuContainer = document.querySelector(".container_menu");
+const plusIcon = document.querySelector(".plus_icon");
+const containerTask = document.querySelector(".container_task");
 
-cuentaIcono.addEventListener('click', () => {
-  iconCerrar.classList.toggle('icon_cerrar--no');
-  menuOverlay.classList.toggle('menu_overlay--oculto');
-  menuContainer.classList.toggle('container_menu--abierto');
-})
+// Abrir menu tocando el perfil
+cuentaIcono.addEventListener("click", () => {
+  iconCerrar.classList.toggle("icon_cerrar--no");
+  menuOverlay.classList.toggle("menu_overlay--oculto");
+  menuContainer.classList.toggle("container_menu--abierto");
+});
 
-menuOverlay.addEventListener('click', () => {
-  iconCerrar.classList.remove('icon_cerrar--no');
-  menuOverlay.classList.remove('menu_overlay--oculto');
-  menuContainer.classList.remove('container_menu--abierto');
-})
+// Cerrar menu o login tocando cualque parte de la pantalla
+menuOverlay.addEventListener("click", () => {
+  iconCerrar.classList.remove("icon_cerrar--no");
+  menuOverlay.classList.remove("menu_overlay--oculto");
+  menuContainer.classList.remove("container_menu--abierto");
+});
 
-// const filaTarea = [];
-// filaTarea.push({
-//   marcador: '../assets/icon/no_marcado_icon.svg',
-//   textTarea: 'Tarea',
-//   tacho: '../assets/icon/basura_icon.svg'
-// });
+// Manejo del DOM para crear cada tarea
+plusIcon.addEventListener("click", () => {
+  const taskRow = document.createElement("div");
+  taskRow.classList.add("task--row");
 
+  const marcador = document.createElement("div");
+  marcador.classList.add("no_marcado--task");
 
-{/* <div class="task--row">
-  <div class="no_marcado--task"></div>
-  <div class="container-text--task">
-      <input class="text--task" placeholder="Tarea" value="">
-  </div>
-  <div class="no_borrado--task"></div>
-</div> */}
+  const task = document.createElement("div");
+  task.classList.add("container-text--task");
 
-plusIcon.addEventListener('click', ()=>{
-  console.log('tiene que crear una tarea');
+  const inputTask = document.createElement("input");
+  inputTask.classList.add("text--task");
+  inputTask.setAttribute("placeholder", "Tarea");
 
-  const taskRow = document.createElement('div');
-  taskRow.classList.add('task--row');
+  const basura = document.createElement("div");
+  basura.classList.add("no_borrado--task");
 
-  const marcador = document.createElement('div');
-  marcador.classList.add('no_marcado--task');
-
-  const task = document.createElement('div');
-  task.classList.add('container-text--task');
-
-  const inputTask = document.createElement('input');
-  inputTask.classList.add('text--task');
-  inputTask.setAttribute('placeholder', 'Tarea');
-
-  
-  const basura = document.createElement('div');
-  basura.classList.add('no_borrado--task');
-  
   containerTask.appendChild(taskRow);
   taskRow.appendChild(marcador);
   taskRow.appendChild(task);
   task.appendChild(inputTask);
   taskRow.appendChild(basura);
 
-  marcarTask();
+  // Marcador de tarea
+
+  const marcadorTarea = document.querySelector(".no_marcado--task");
+  const textTarea = document.querySelector(".text--task");
+  const tarea = document.querySelector('.task--row');
+
+  marcadorTarea.addEventListener('click', (e)=>{
+    const item = e.target.parentElement;
+    tarea.classList.toggle(item);
+  })
+
+
+
+
+
+  // const marcadorTarea = document.querySelectorAll('.no_marcado--task');
+  // const textTarea = document.querySelectorAll('.text--task');
+
+  // for (let i = 0; i < marcadorTarea.length; i++) {
+  //   const marcador = marcadorTarea[i];
+  //   const textoTarea = textTarea[i];
+
+  //   marcador.addEventListener('click', ()=>{
+  //     marcador.classList.toggle('marcado--task');
+  //     textoTarea.classList.toggle('text--task_resuelto');
+
+  //     console.log(i);
+  //     console.log(marcadorTarea[i]);
+  //     console.log(marcadorTarea.length + '.length');
+  //   });
+
+  // }
 });
-
-function marcarTask() {
-  const marcadorTarea = document.querySelector('.no_marcado--task');
-  const textTarea = document.querySelector('.text--task');
-  const borrarTask = document.querySelector('.no_borrado--task');
-  const taskRow = document.querySelector('.task--row');
-
-  marcadorTarea.addEventListener('click', ()=>{
-    marcadorTarea.classList.toggle('marcado--task');
-    textTarea.classList.toggle('text--task_resuelto');
-  });
-
-  borrarTask.addEventListener('click', ()=>{
-    taskRow.parentNode.removeChild(taskRow);
-  });
-};
